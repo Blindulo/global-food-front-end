b972cfbff837e1116f6dea258f514eedab34de9f
Xiya Liu <xil230@pitt.edu>
2020-04-14 11:14:59 -0400

    market_type

--- adv_frontend/src/app/models/Query.ts
+++ adv_frontend/src/app/models/Query.ts
@@ -1,10 +1,5 @@
 export interface Query{
     country?:String,
     market?:String,
-    product_name?:String,
-    performance?:String,
-    conf?:String,
-    time?:String,
-    rank?:String,
-    opponent?:String
+    product_name?:String
 }
